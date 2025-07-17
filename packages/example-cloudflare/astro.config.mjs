a654676101b1a8efc652afcea9d256507c794b97
Nookery <nooks@qq.com>
2025-07-17 23:25:42 +0800

    🆕 Feature: Add multilingual support and config structure for Cloudflare example

--- packages/example-cloudflare/astro.config.mjs
+++ packages/example-cloudflare/astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import path from 'path';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
