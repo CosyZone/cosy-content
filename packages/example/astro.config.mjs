62b6ca620e5a9b2e223cfccd214d68f51ca1009e
nookery <qdayy@think-land.com>
2025-07-18 09:39:12 +0800

    🗑️ Trash: Remove build_web_toolbox lessons and related files

--- packages/example/astro.config.mjs
+++ packages/example/astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import path from 'path';
 
+import mdx from '@astrojs/mdx';
+
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: node({ mode: 'standalone' }),
+
   vite: {
     resolve: {
       alias: {
@@ -13,4 +18,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [mdx(), vue()],
 });
